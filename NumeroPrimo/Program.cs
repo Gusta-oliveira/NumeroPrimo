f8d2b46f3173b5242634a8e3c7cf31a4c6bb7d5b
Gustavo Oliveira <gustavo.ramosue@gmail.com>
2023-03-06 00:50:21 -0300

    Add project files.

---
+++ NumeroPrimo/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double digit_u;
+        int count, primo = 0;
+
+        Console.WriteLine("Informe o digito: ");
+        digit_u = double.Parse(Console.ReadLine());
+
+        for (count = 1; count <= digit_u; count++)
+        {
+        if (digit_u % count == 0)
+        {
+            primo++;
+        }
+        }
+        if (primo == 2)
+        {
+            Console.WriteLine("Número primo");
+        }
+        else
+        {
+            Console.WriteLine("Numero não é primo");
+        }
+    }
+}
